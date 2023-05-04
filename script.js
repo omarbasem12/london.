37a8dd92074c9e6ab147f6fc8de8d807d013104a
omarbasem12 <132420122+omarbasem12@users.noreply.github.com>
2023-05-04 15:34:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function scrollElement() {
+    var element = document.getElementById('history-london');
+    element.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollLandmarks() {
+    var landmarks = document.getElementById('Landmarks')
+    landmarks.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollFood() {
+    var food = document.getElementById('Food')
+    food.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollCulture() {
+    var Culture = document.getElementById('culture')
+    Culture.scrollIntoView({ behavior: 'smooth' }, true);
+}
+
+function scrollLocation() {
+    var location = document.getElementById('location')
+    location.scrollIntoView({ behavior: 'smooth' }, true);
+}
